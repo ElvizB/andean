66311f3efce4ca8a5ac36428d92a3b20729e235c
ElvisB <sagitario12>
2019-09-16 12:20:02 -0500

    [update] .gitignore

--- app/config.inc.php
+++ app/config.inc.php
@@ -7,7 +7,7 @@ define('NOMBRE_BD', 'andean');
 
 //rutas de la web
 
-define("SERVIDOR", "http://localhost/andean");
+define("SERVIDOR", "https://andean.herokuapp.com/");
 define("RUTA_NOSOTROS", SERVIDOR."/Nosotros");
 define("RUTA_DESTINOS", SERVIDOR."/Destinos");
 define("RUTA_CONTACTO", SERVIDOR."/Contacto");
